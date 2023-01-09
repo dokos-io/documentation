f329542a4b231dc3e8e18023a37788b01bd251b0
Charles-Henri Decultot <chdecultot@dokos.io>
2023-01-09 16:14:07 +0100

    feat: Add tokens config file

---
+++ tokens.config.ts
@@ -0,0 +1,47 @@
+import { defineTheme } from 'pinceau'
+import nuxtThemeTokens from '@nuxt-themes/tokens/tokens'
+
+export default defineTheme({
+  color: {
+    primary: (nuxtThemeTokens.color as any).lightblue
+  },
+  docus: {
+    $schema: {
+      title: 'All the configurable tokens from Docus.',
+      tags: [
+        '@studio-icon material-symbols:docs'
+      ]
+    },
+    header: { height: '64px' },
+    footer: { padding: '{space.4} 0' },
+    readableLine: '117ch'
+  },
+  typography: {
+    color: {
+      primary: {
+        50: '{color.primary.50}',
+        100: '{color.primary.100}',
+        200: '{color.primary.200}',
+        300: '{color.primary.300}',
+        400: '{color.primary.400}',
+        500: '{color.primary.500}',
+        600: '{color.primary.600}',
+        700: '{color.primary.700}',
+        800: '{color.primary.800}',
+        900: '{color.primary.900}'
+      },
+      secondary: {
+        50: '{color.gray.50}',
+        100: '{color.gray.100}',
+        200: '{color.gray.200}',
+        300: '{color.gray.300}',
+        400: '{color.gray.400}',
+        500: '{color.gray.500}',
+        600: '{color.gray.600}',
+        700: '{color.gray.700}',
+        800: '{color.gray.800}',
+        900: '{color.gray.900}'
+      }
+    }
+  }
+})
