b53b38c9d73a5c1c0183650bd59de9bb0d307af6
Corentin Forler <8860073-cforler_dokos@users.noreply.gitlab.com>
2023-01-18 14:40:36 +0100

    feat: Add link cards in homepage

---
+++ components/content/LinkCard.vue
@@ -0,0 +1,99 @@
+<script setup lang="ts">
+defineProps({
+  icon: {
+    type: String,
+    default: ''
+  },
+  iconClass: {
+    type: String,
+    default: ''
+  },
+  blurry: {
+    type: Boolean,
+    default: true,
+    required: false
+  },
+  href: {
+    type: String,
+    default: ''
+  },
+})
+</script>
+
+<template>
+  <NuxtLink tag="div" :to="href" :class="{ blurry }" class="card LinkCard">
+    <Icon v-if="icon" :name="icon" class="LinkCard--icon" />
+    <slot />
+    <div>
+      <h3 class="title">
+        <ContentSlot :use="$slots.title" unwrap="p">
+          Card title
+        </ContentSlot>
+      </h3>
+      <p class="description">
+        <ContentSlot :use="$slots.description" unwrap="p">
+          Card description
+        </ContentSlot>
+        <div class="LinkCard--link">
+          <span>Documentation</span>
+          <Icon name="material-symbols:arrow-right-alt-rounded"></Icon>
+        </div>
+      </p>
+    </div>
+  </NuxtLink>
+</template>
+
+<style scoped lang="ts">
+css({
+  '.card': {
+    display: 'block',
+    position: 'relative',
+    width: '100%',
+    padding: '{space.8}',
+    borderRadius: '{radii.xl}',
+    border: '1px solid {elements.border.primary.default}',
+    background: '{elements.backdrop.background}',
+    transition: 'all 300ms ease',
+    '@dark': {
+      color: '{color.gray.50}'
+    },
+    '&.blurry': {
+      backdropFilter: '{elements.backdrop.filter}',
+    },
+    '.LinkCard--icon': {
+      display: 'inline-block',
+      marginBottom: '{space.2}',
+      width: '{space.6}',
+      height: '{space.6}'
+    },
+    '.title': {
+      marginBottom: '{space.2}',
+      fontSize: '{text.lg.fontSize}',
+      lineHeight: '{text.lg.lineHeight}',
+      fontWeight: '{fontWeight.semibold}'
+    },
+    '.description': {
+      fontSize: '{text.sm.fontSize}',
+      lineHeight: '{text.sm.lineHeight}',
+      fontWeight: '{fontWeight.medium}',
+      color: '{elements.color.text.secondary}',
+      marginBottom: '{space.4}',
+    },
+    '.LinkCard--link': {
+      display: 'block',
+      position: 'absolute',
+      bottom: '{space.6}',
+      right: '{space.6}',
+      color: '{elements.color.text.secondary.default}',
+      borderBottom: '1px dashed currentColor',
+    },
+    '&:hover': {
+      border: '1px solid {elements.color.text.primary.default}',
+      '.LinkCard--link': {
+        color: '{elements.color.text.primary.default}',
+        borderBottom: '1px solid currentColor',
+      },
+    },
+  }
+})
+</style>
