71c8e12ff4e161cfcf614e230d32d683f04ee411
Corentin Forler <8860073-cforler_dokos@users.noreply.gitlab.com>
2023-04-06 22:55:34 +0200

    feat: Async mermaid

--- mermaid.client.js
+++ mermaid.client.js
@@ -1,5 +1,9 @@
 import mermaid from "mermaid"
 
 export default defineNuxtPlugin((nuxtApp) => {
+    mermaid.initialize({
+        startOnLoad: false,
+        securityLevel: 'loose',
+    })
     nuxtApp.provide('mermaid', () => mermaid)
 })
