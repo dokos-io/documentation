23629bd24e7d2e3705f451ffd778a5cdf6e3cee8
Charles-Henri Decultot <chdecultot@dokos.io>
2023-04-07 12:40:06 +0200

    chore: Initialize translations for DocSearch

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,48 @@
 // https://nuxt.com/docs/getting-started/deployment#static-hosting
+
+const translations = {
+  button: {
+    buttonText: 'Recherche',
+    buttonAriaLabel: 'Recherche',
+  },
+  modal: {
+    searchBox: {
+      resetButtonTitle: 'Clear the query',
+      resetButtonAriaLabel: 'Clear the query',
+      cancelButtonText: 'Cancel',
+      cancelButtonAriaLabel: 'Cancel',
+    },
+    startScreen: {
+      recentSearchesTitle: 'Recherches récentes',
+      noRecentSearchesText: 'Aucune recherche récente',
+      saveRecentSearchButtonTitle: 'Save this search',
+      removeRecentSearchButtonTitle: 'Remove this search from history',
+      favoriteSearchesTitle: 'Favorite',
+      removeFavoriteSearchButtonTitle: 'Remove this search from favorites',
+    },
+    errorScreen: {
+      titleText: 'Unable to fetch results',
+      helpText: 'You might want to check your network connection.',
+    },
+    footer: {
+      selectText: 'pour sélectionner',
+      selectKeyAriaLabel: 'Enter key',
+      navigateText: 'pour naviguer',
+      navigateUpKeyAriaLabel: 'Arrow up',
+      navigateDownKeyAriaLabel: 'Arrow down',
+      closeText: 'pour fermer',
+      closeKeyAriaLabel: 'Escape key',
+      searchByText: 'Recherche proposée par',
+    },
+    noResultsScreen: {
+      noResultsText: 'Aucun résultat pour',
+      suggestedQueryText: 'Try searching for',
+      reportMissingResultsText: 'Believe this query should return results?',
+      reportMissingResultsLinkText: 'Let us know.',
+    },
+  },
+};
+
 export default defineNuxtConfig({
   ssr: true,
   modules: [
@@ -38,8 +82,10 @@ export default defineNuxtConfig({
         applicationId: process.env.DOCSEARCH_APP_ID,
         apiKey: process.env.DOCSEARCH_API_KEY,
         langAttribute: 'lang',
+        translations: translations,
+        placeholder: "Rechercher un article",
         docSearch: {
-          indexName: 'dokos'
+          indexName: 'dokos',
         }
       }
     }
