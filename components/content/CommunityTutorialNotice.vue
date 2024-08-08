7c30998a4f3af78bd02d2250fae60080215ec247
Corentin Forler <8860073-cforler_dokos@users.noreply.gitlab.com>
2024-08-08 17:07:41 +0200

    doc: Réorganiser les méthodes d'installation communautaires

---
+++ components/content/CommunityTutorialNotice.vue
@@ -0,0 +1,6 @@
+<template>
+  <Callout>
+    Cet article a été rédigé avec <span aria-label="amour" title="amour">❤️</span> par un membre de la communauté Dokos.
+    Il est maintenu la communauté et peut donc ne pas être à jour.
+  </Callout>
+</template>
